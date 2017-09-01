43992ddd65cc83d54799cc93a80851bd0720f77d
Widya Puspitaloka <widyapuspitaloka@zeniuseducation.com>
2017-09-01 02:56:25 +0200

    Add files via upload

--- database_setup.py
+++ database_setup.py
@@ -1,40 +1,38 @@
+import os
+import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-
 Base = declarative_base()
 
 class User(Base):
-   __tablename__ = 'user'
+    __tablename__ = 'user'
 
-   id = Column(Integer, primary_key=True)
-   name = Column(String(250), nullable=False)
-   email= Column(String(250), nullable=False)
-   picture = Column(String(250))
-
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
 
 class Album(Base):
-   __tablename__ = 'album'
+    __tablename__ = 'album'
 
-   id = Column(Integer, primary_key=True)
-   name = Column(String(250), nullable=False)
-   user_id = Column(Integer, ForeignKey('user.id'))
-   user = relationship(User)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
-   @property
-   def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-      }
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.user_id,
+        }
 
-
-class songItem(Base):
+class SongItem(Base):
     __tablename__ = 'song_item'
-
 
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
@@ -42,23 +40,22 @@ class songItem(Base):
     length = Column(String(8))
     genre = Column(String(250))
     album_id = Column(Integer,ForeignKey('album.id'))
-    album = relationship('Album')
+    album = relationship(Album)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
+# We added this serialize function to be able to send JSON objects in a
+# serializable format
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'year'         : self.year,
-           'id'           : self.id,
-           'length'       : self.length,
-           'genre'        : self.genre,
-       }
 
+        return {
+            'name': self.name,
+            'year': self.year,
+            'id': self.id,
+            'length': self.length,
+            'genre': self.genre,
+        }
 
-engine = create_engine('sqlite:///coldplaydiscography.db')
-
-
+engine = create_engine('sqlite:///coldplaydiscographywithuser.db')
 Base.metadata.create_all(engine)
